c0cf5c9f820516b9d4f332a5008bb42912b0f2e9
Yoann WACKERS <y.wackers@highco-data.fr>
2014-11-21 11:41:31 +0100

    Add unit tests to several asserters

---
+++ tests/units/Test/Asserters/RecursiveArray.php
@@ -0,0 +1,98 @@
+<?php
+namespace atoum\AtoumBundle\tests\units\Test\Asserters;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+use mageekguy\atoum;
+use mageekguy\atoum\asserter;
+use atoum\AtoumBundle\Test\Asserters\RecursiveArray as TestedClass;
+
+class RecursiveArray extends atoum\test
+{
+    public function testClass()
+    {
+        $this->testedClass->isSubclassOf('\\mageekguy\\atoum\\asserters\\phpArray');
+    }
+
+    public function test__construct()
+    {
+        $this
+            ->if($object = new TestedClass($generator = new asserter\generator()))
+            ->then
+                ->object($object->getLocale())->isIdenticalTo($generator->getLocale())
+                ->object($object->getGenerator())->isIdenticalTo($generator)
+        ;
+    }
+
+    public function testHasArray()
+    {
+        $this
+            ->if($object = new TestedClass($generator = new asserter\generator()))
+            ->and($array = ['key' => []])
+            ->and($object->setWith($array, true))
+            ->then
+                ->class($object->hasArray('key'))->isSubclassOf('\\mageekguy\\atoum\\asserters\\phpArray')
+            ->if($array = ['key' => 'NotAnArray'])
+            ->and($object->setWith($array, true))
+            ->then
+                ->exception(function () use ($object) {
+                    $object->hasArray('key');
+                })
+                    ->isInstanceOf('mageekguy\atoum\asserter\exception')
+        ;
+    }
+
+    public function testHasString()
+    {
+        $this
+            ->if($object = new TestedClass($generator = new asserter\generator()))
+            ->and($array = ['key' => 'string'])
+            ->and($object->setWith($array, true))
+            ->then
+                ->class($object->hasString('key'))->isSubclassOf('\\mageekguy\\atoum\\asserters\\string')
+            ->if($array = ['key' => []])
+            ->and($object->setWith($array, true))
+            ->then
+                ->exception(function () use ($object) {
+                    $object->hasString('key');
+                })
+                    ->isInstanceOf('mageekguy\atoum\asserter\exception')
+        ;
+    }
+
+    public function testHasInteger()
+    {
+        $this
+            ->if($object = new TestedClass($generator = new asserter\generator()))
+            ->and($array = ['key' => 42])
+            ->and($object->setWith($array, true))
+            ->then
+                ->class($object->hasInteger('key'))->isSubclassOf('\\mageekguy\\atoum\\asserters\\integer')
+            ->if($array = ['key' => []])
+            ->and($object->setWith($array, true))
+            ->then
+                ->exception(function () use ($object) {
+                    $object->hasInteger('key');
+                })
+                    ->isInstanceOf('mageekguy\atoum\asserter\exception')
+        ;
+    }
+
+    public function testHasFloat()
+    {
+        $this
+            ->if($object = new TestedClass($generator = new asserter\generator()))
+            ->and($array = ['key' => 42.12])
+            ->and($object->setWith($array, true))
+            ->then
+                ->class($object->hasFloat('key'))->isSubclassOf('\\mageekguy\\atoum\\asserters\\float')
+            ->if($array = ['key' => []])
+            ->and($object->setWith($array, true))
+            ->then
+                ->exception(function () use ($object) {
+                    $object->hasFloat('key');
+                })
+                    ->isInstanceOf('mageekguy\atoum\asserter\exception')
+        ;
+    }
+}
---
+++ tests/units/Test/Asserters/String.php
@@ -0,0 +1,16 @@
+<?php
+namespace atoum\AtoumBundle\tests\units\Test\Asserters;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+use mageekguy\atoum;
+use mageekguy\atoum\asserter;
+use atoum\AtoumBundle\Test\Asserters\String as TestedClass;
+
+class String extends atoum\test
+{
+    public function testClass()
+    {
+        $this->testedClass->isSubclassOf('\\mageekguy\\atoum\\asserters\\string');
+    }
+}
